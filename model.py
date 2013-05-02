2c4ce1c48a4e73a4b5d3d6fe044c6b7db59d0b9b
Nils Dagsson Moskopp <nils@dieweltistgarnichtso.net>
2013-05-02 23:18:54 +0200

    + model for refefe comments

---
+++ model.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from datetime import datetime
+
+class refefeComment(object):
+    def __init__(self, ts, fefets, nick, text, ip, censored):
+        self.ts = datetime.strptime(ts, '%Y-%m-%d %H:%M:%S')
+        self.fefets = fefets
+        self.nick = nick.decode('utf-8')
+        self.text = text.decode('utf-8')
+        self.ip = ip
+        self.censored = bool(censored)
+
+class refefeModel(object):
+    def __init__(self, filename):
+
+        with open(filename) as f:
+            content = f.read()
+            rows = [r.split('\t') for r in content.split('NULL\n')[1:]]
+            self.comments = [refefeComment(
+                    row[1], row[2], row[3], row[4], row[5], row[6],
+                    ) for row in rows]
