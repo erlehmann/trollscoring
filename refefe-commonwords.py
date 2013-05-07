a2ff62a12db017dd1276a2908f85e16711b8956e
Nils Dagsson Moskopp <nils@dieweltistgarnichtso.net>
2013-05-08 00:56:41 +0200

    * fix common word count

--- refefe-commonwords.py
+++ refefe-commonwords.py
@@ -12,7 +12,7 @@ from sys import stdout
 with open('top100de.utf8.txt') as f:
     common_words = [word.strip().decode('utf-8') for word in f.readlines()]
 
-for sentence in (comment.html for comment in m.comments):
+for sentence in (comment.text for comment in m.comments):
     for word in sentence.split():
         if (word[0].upper() != word[0]) or (len(word) == 1) or \
                 (word.lower() in common_words):
