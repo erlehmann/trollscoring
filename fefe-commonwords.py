cd08f1d097293b90a23da7cde093281c1a1aad06
Nils Dagsson Moskopp <nils@dieweltistgarnichtso.net>
2013-05-07 21:14:39 +0200

    * use text instead of html

--- fefe-commonwords.py
+++ fefe-commonwords.py
@@ -12,7 +12,7 @@ from sys import stdout
 with open('top100de.utf8.txt') as f:
     common_words = [word.strip().decode('utf-8') for word in f.readlines()]
 
-for sentence in (post.html for post in m.posts):
+for sentence in (post.text for post in m.posts):
     for word in sentence.split():
         if (word[0].upper() != word[0]) or (len(word) == 1) or \
                 (word.lower() in common_words):
